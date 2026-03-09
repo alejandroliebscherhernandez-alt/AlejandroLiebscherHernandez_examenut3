72d554c3edbe945375516c2e8a947f32e5daac9e
Alejandro Liebscher Hernández <alejandroliebscherhernandez@alumno.ieselrincon.es>
2026-03-09 10:22:15 +0000

    Entrega 2/2

--- src/es/globalpack/modelos/Paquete.java
+++ src/es/globalpack/modelos/Paquete.java
@@ -3,21 +3,19 @@ package es.globalpack.modelos;
 import es.globalpack.modelos.detalles.Direccion;
 
 public class Paquete extends Envio implements Asegurable {
-    private double volumen; // Atributo propio [cite: 36]
+    private double volumen;
 
-    public Paquete(String idSeguimiento, double peso, Direccion destino, double volumen) {
-        super(idSeguimiento, peso, destino); // Llamada al constructor base [cite: 35]
+    public Paquete(String idSeguimiento, double peso, Direccion destino) {
+        super(idSeguimiento, peso, destino);
         this.volumen = volumen;
     }
 
     @Override
     public double calcularPrecioFinal() {
-        double precio = 5.0; // Precio base [cite: 42]
-        // Lógica por peso extra [cite: 43]
+        double precio = 5.0;
         if (this.peso > 5) {
             precio += (this.peso - 5) * 2.0;
         }
-        // Lógica por volumen [cite: 44]
         if (this.volumen > 1) {
             precio += 10.0;
         }
@@ -26,6 +24,6 @@ public class Paquete extends Envio implements Asegurable {
 
     @Override
     public double calcularSeguro(double valorMercancia) {
-        return valorMercancia * 0.03; // Seguro del 3% [cite: 51]
+        return valorMercancia * 0.03;
     }
 }
